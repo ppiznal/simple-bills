803df7f004b9abcfb21000e00a72e9bdbbfcb309
lseremak <llseremak@gmail.com>
2022-11-23 23:42:57 +0100

    update docker-compose.yml

--- simple-bills-gui/src/environments/environment.prod.ts
+++ simple-bills-gui/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  transactionManagementHost: "http://localhost:8080",
+  planningHost: "http://localhost:8081",
+  assetManagementHost: "http://localhost:8082",
+  redirectUri: "http://localhost:8089/",
+  tokenUrl: "http://localhost:8085/realms/simple-bills/protocol/openid-connect/token",
+  authUrl: "http://localhost:8085/realms/simple-bills/protocol/openid-connect/auth"
 };
