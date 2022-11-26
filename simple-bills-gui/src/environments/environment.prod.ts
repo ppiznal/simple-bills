512e4555b755ee37894028e838ec128606a46aa6
lseremak <llseremak@gmail.com>
2022-11-26 18:51:24 +0100

    Update siple-bills-gui config

--- simple-bills-gui/src/environments/environment.prod.ts
+++ simple-bills-gui/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  transactionManagementHost: "http://localhost:8080",
-  planningHost: "http://localhost:8081",
-  assetManagementHost: "http://localhost:8082",
-  redirectUri: "http://localhost:8089/",
-  tokenUrl: "http://localhost:8085/realms/simple-bills/protocol/openid-connect/token",
-  authUrl: "http://localhost:8085/realms/simple-bills/protocol/openid-connect/auth"
+  transactionManagementHost: "https://transaction-management-lukaszse.cloud.okteto.net",
+  planningHost: "https://planning-lukaszse.cloud.okteto.net",
+  assetManagementHost: "https://asset-management-lukaszse.cloud.okteto.net",
+  redirectUri: "https://simple-bills-gui-lukaszse.cloud.okteto.net\"",
+  tokenUrl: "https://37.233.102.95:8443/realms/simple-bills/protocol/openid-connect/token",
+  authUrl: "https://37.233.102.95:8443/realms/simple-bills/protocol/openid-connect/auth"
 };
