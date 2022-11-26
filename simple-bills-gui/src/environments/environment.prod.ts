850f6bbce3616c8b2f1d533f5a57a41314758ac0
lseremak <llseremak@gmail.com>
2022-11-26 19:25:04 +0100

    update simple-bills config

--- simple-bills-gui/src/environments/environment.prod.ts
+++ simple-bills-gui/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   transactionManagementHost: "https://transaction-management-lukaszse.cloud.okteto.net",
   planningHost: "https://planning-lukaszse.cloud.okteto.net",
   assetManagementHost: "https://asset-management-lukaszse.cloud.okteto.net",
-  redirectUri: "https://simple-bills-gui-lukaszse.cloud.okteto.net",
+  redirectUri: "https://simple-bills-gui-lukaszse.cloud.okteto.net/",
   tokenUrl: "https://37.233.102.95:8443/realms/simple-bills/protocol/openid-connect/token",
   authUrl: "https://37.233.102.95:8443/realms/simple-bills/protocol/openid-connect/auth"
 };
