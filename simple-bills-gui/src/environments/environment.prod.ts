f9322b33ed9a8f28d833566707ba2942015fe72a
lseremak <llseremak@gmail.com>
2022-11-27 13:12:44 +0100

    simple-bills-gui - change production hosts

--- simple-bills-gui/src/environments/environment.prod.ts
+++ simple-bills-gui/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   planningHost: "https://planning-lukaszse.cloud.okteto.net",
   assetManagementHost: "https://asset-management-lukaszse.cloud.okteto.net",
   redirectUri: "https://simple-bills-gui-lukaszse.cloud.okteto.net/",
-  tokenUrl: "https://37.233.102.95:8443/realms/simple-bills/protocol/openid-connect/token",
-  authUrl: "https://37.233.102.95:8443/realms/simple-bills/protocol/openid-connect/auth"
+  tokenUrl: "https://keycloak-lukaszse.cloud.okteto.net/realms/simple-bills/protocol/openid-connect/token",
+  authUrl: "https://keycloak-lukaszse.cloud.okteto.net/realms/simple-bills/protocol/openid-connect/auth"
 };
